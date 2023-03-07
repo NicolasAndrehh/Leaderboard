5babbb82f63a1fe375acc515e1c1b3344fc733dc
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-07 13:12:32 -0500

    Updates

--- runtime.bundle.js
+++ runtime.bundle.js
@@ -158,7 +158,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkLeaderboard"] = self["webpackChunkLeaderboard"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkleaderboard"] = self["webpackChunkleaderboard"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
